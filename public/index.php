878bf8b5500a3519d0af5475387430b9fdb450b2
oscar <oscarch110622@gmail.com>
2025-03-17 13:05:05 -0600

    firs commit

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola </h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sin
+        t magnam necessitatibus perspiciatis aperiam dolorem praesentium odio magni sunt a eaque! Doloremque similique sapiente
+         architecto fugiat quisquam sint rerum magnam voluptatum.</p>
+</body>
+</html>
